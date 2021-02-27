f6e0a417a9cd8d4328ebae5a96cfbc73f3c49875
Mikhail Shtainer <63235817+FANDIM-wolf@users.noreply.github.com>
2021-02-27 11:57:45 +0300

    Add files via upload

---
+++ BASEnet/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace BASEnet
+{
+    class ApplicationContext:DbContext
+    {
+        public DbSet<User> Users { get; set; } //list , model User , table Users
+
+        public ApplicationContext() : base("DefaultConnection") { } //settings of base data has name "DefaultConnection"
+    }
+}
